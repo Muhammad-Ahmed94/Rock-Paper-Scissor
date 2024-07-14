e451e9c21a2c106a27d3104301a67c2de24ece3e
Muneeb Ahmed94 <muneebbum757@gmail.com>
2024-07-14 12:43:35 +0500

    simple rock paper scissor game using some JS logic

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-const display = document.querySelector(".display-value");
+const playerText = document.querySelector(".playerText");
+const computerText = document.querySelector(".computerText");
+const result = document.querySelector(".result");
 
-function getValue(val) {
-  if (
-    display.value == "input required" ||
-    display.value == "Error: Invalid Operation" ||
-    display.value == "Infinity"
-  ) {
-    display.value = "";
+let player;
+let computer;
+
+function playGame(playerChoice) {
+  player = playerChoice;
+  winner();
+  if(player === computer) {
+    result.textContent = "Tie";
   }
-  display.value += val;
-}
-
-function calculate() {
-  try {
-    if (display.value == "") {
-      display.value = "input required";
-    } else {
-      display.value = eval(display.value);
-    }
-  } catch (err) {
-    display.value = "Error: Invalid Operation";
+  switch (player) {
+    case "Rock":
+      result.textContent = (computer === "Paper") ? "you lose" : "You won";
+      break;
+    case "Paper":
+      result.textContent = (computer === "Scissor") ? "you lose" : "You won";
+      break;
+    case "Scissor":
+      result.textContent = (computer === "Rock") ? "you lose" : "You won";
+      break;
   }
+  playerText.textContent = `Player Picked: ${player}`;
+  computerText.textContent = `Computer Picked: ${computer}`;
 }
 
-function clearDisplay() {
-  display.value = "";
+function winner() {
+  computer = Math.floor(Math.random() * 3);
+  switch (computer) {
+    case 0:
+      computer = "Rock";
+      break;
+    case 1:
+      computer = "Paper";
+      break;
+    case 2:
+      computer = "Scissor";
+      break;
+  }
+  return computer;
 }
+
+
